2219a5548bfbf04ad068e0cfe45b0461db56727e
Andrew Koziulia <akoziulia@cogniance.com>
2017-03-14 20:24:32 +0200

    JS. Homework 19-20. More scss

--- src/js/homework19_20/build_home/src/js/script.js
+++ src/js/homework19_20/build_home/src/js/script.js
@@ -1,5 +1,14 @@
 $(function () {
 
+    $('.carousel').jcarousel();
 
+    $('.jcarousel-pagination')
+        .on('jcarouselpagination:active', 'a', function () {
+            $(this).addClass('active');
+        })
+        .on('jcarouselpagination:inactive', 'a', function () {
+            $(this).removeClass('active');
+        })
+        .jcarouselPagination();
 
 });
