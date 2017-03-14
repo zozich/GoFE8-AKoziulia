3ee69fe554885e072e0ca83dabf1d7161c69bce4
a.koziulia <zozich2010@gmail.com>
2017-03-15 01:56:41 +0200

    JS. Homework 19-20. Final scss sketch + fonts

--- src/js/homework19_20/build_home/gulpfile.js
+++ src/js/homework19_20/build_home/gulpfile.js
@@ -5,24 +5,25 @@ var concat = require('gulp-concat');
 var clean = require('gulp-clean');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
+var sequence = require('gulp-sequence');
 
 gulp.task('build-css', function () {
-    return gulp.src(['src/lib/**/*.css', 'src/css/*.css'])
+    return gulp.src(['src/lib/**/*.css', 'src/scss/*.scss'])
         .pipe(concat('styles.css'))
         .pipe(sass())
-        .pipe(gulp.dest('dist/styles'))
+        .pipe(gulp.dest('dist/css'))
         .pipe(cleanCss())
         .pipe(rename('styles.min.css'))
         .pipe(gulp.dest('dist/css'))
 });
 
 gulp.task('build-js', function () {
-    return gulp.src(['src/lib/**/*.js', 'src/js/*.js'])
+    return gulp.src(['src/lib/jquery/*.js', 'src/lib/**/*.js', 'src/js/*.js'])
         .pipe(concat('script.js'))
         .pipe(gulp.dest('dist/js'))
         .pipe(uglify())
         .pipe(rename('script.min.js'))
-        .pipe(gulp.dest('dist/js'))
+        .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('clean', function () {
@@ -32,12 +33,19 @@ gulp.task('clean', function () {
 
 gulp.task('copy-images', function () {
     return gulp.src('src/img/**/**')
-        .pipe(gulp.dest('dist/img'))
+        .pipe(gulp.dest('dist/img'));
 });
 
 gulp.task('copy-fonts', function () {
     return gulp.src('src/fonts/**/**')
-        .pipe(gulp.dest('dist/fonts'))
+        .pipe(gulp.dest('dist/fonts'));
 });
 
-gulp.task('default', ['clean', 'build-css', 'build-js', 'copy-images', 'copy-fonts']);
+gulp.task('watch', function () {
+    gulp.watch('src/scss/**/*.scss', ['build-css']);
+    gulp.watch('src/js/**/*.js', ['build-js']);
+});
+
+gulp.task('default', function (cb) {
+    sequence('clean', ['build-css', 'build-js', 'copy-images', 'copy-fonts'])(cb);
+});
