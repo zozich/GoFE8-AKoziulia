26b5bb556d9bd1838941d7a1c0da1e6706db866e
a.koziulia <zozich2010@gmail.com>
2017-03-26 23:09:01 +0300

    React offline

---
+++ src/offlines/offline_25_03_2017/js/App.js
@@ -0,0 +1,98 @@
+var my_news = [
+    {
+        author: 'Саша Печкин',
+        text: 'В четчерг, четвертого числа...',
+        bigText: 'в четыре с четвертью часа четыре чёрненьких чумазеньких чертёнка чертили чёрными чернилами чертёж.'
+    },
+    {
+        author: 'Просто Вася',
+        text: 'Считаю, что $ должен стоить 35 рублей!',
+        bigText: 'А евро 42!'
+    },
+    {
+        author: 'Гость',
+        text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000',
+        bigText: 'На самом деле платно, просто нужно прочитать очень длинное лицензионное соглашение'
+    }
+];
+
+var News = React.createClass({
+    propTypes: {
+        data: React.PropTypes.array.isRequired
+    },
+    render: function () {
+        var data = this.props.data;
+        var newsTemplate;
+        if (data.length > 0) {
+            newsTemplate = data.map(function (item, index) {
+                return (
+                    <div key={index}>
+                        <Article data={item}/>
+                    </div>
+                )
+            })
+        } else {
+            newsTemplate = <p>К сожалению новостей нет</p>
+        }
+        return (
+            <div className='news'>
+                {newsTemplate}
+                <strong className={'news__count ' + (data.length > 0 ? '':'none') }>Всего
+                    новостей: {data.length}</strong>
+            </div>
+        );
+    }
+});
+
+var Article = React.createClass({
+    propTypes: {
+        data: React.PropTypes.shape({
+            author: React.PropTypes.string.isRequired,
+            text: React.PropTypes.string.isRequired,
+            bigText: React.PropTypes.string.isRequired
+        })
+    },
+    getInitialState: function () {
+        return {
+            visible: false
+        };
+    },
+    readmoreClick: function(e) {
+        e.preventDefault();
+        this.setState({visible: true});
+    },
+    render: function () {
+        var author = this.props.data.author,
+            text = this.props.data.text,
+            bigText = this.props.data.bigText,
+            visible = this.state.visible;
+
+        return (
+            <div className='article'>
+                <p className='news__author'>{author}:</p>
+                <p className='news__text'>{text}</p>
+                <a href="#" onClick={this.readmoreClick} className={'news__readmore ' + (visible ? 'none': '')}>
+                    Подробнее
+                </a>
+                <p className={'news__big-text ' + (visible ? '': 'none')}>{bigText}</p>
+
+            </div>
+        )
+    }
+
+});
+
+var App = React.createClass({
+    render: function () {
+        return (
+            <div className="app">
+                <h3>Новости</h3>
+                <News data={my_news}/>
+            </div>
+        );
+    }
+});
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
