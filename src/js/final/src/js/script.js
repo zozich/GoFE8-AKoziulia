409ba096d2a2310975041407927673b45bbc32b4
a.koziulia <zozich2010@gmail.com>
2017-03-31 00:56:05 +0300

    JS Final Exam. Finalized SCSS + Masonry + responsive design

--- src/js/final/src/js/script.js
+++ src/js/final/src/js/script.js
@@ -4,18 +4,44 @@ $(function () {
         itemSelector: '.ideas__item'
     });
 
-    $.ajax({
-        url: "https://pixabay.com/api/?key=4967317-cf3933732a04c09ee425d3446&q=yellow+flowers&image_type=photo",
-        success: function (data) {
-            console.log(data);
-        },
-        dataType: 'json',
-        error: function (error) {
-            console.log(error);
-        },
-        beforeSend: setHeaders
-    });
+    var topics = ['Sport and Activity', 'Wellness and Health', 'Extreme Sports and Expeditions',
+        'Games', 'Culture and Education', 'Relaxation', 'Travelling'];
+    var ideaIds = ['idea0', 'idea1', 'idea2', 'idea3', 'idea4', 'idea5', 'idea6']
+    var imageGetUrl = "https://pixabay.com/api/?key=4967317-cf3933732a04c09ee425d3446&orientation=horizontal&image_type=photo&per_page=7&min_width=620&min_height=310&q=";
 
-    $grid.masonry('layout');
+    for (var i = 0; i < topics.length; i++) {
+        //replaceImages(topics[i], i);
+    }
+
+    function replaceImages(searchKey, imagePos) {
+        $.ajax({
+            url: imageGetUrl + encodeURIComponent(searchKey),
+            success: function (data) {
+                if (imagePos) {
+                    $('#' + ideaIds[imagePos]).attr('src', data.hits[0].webformatURL);
+                    $('#' + ideaIds[imagePos] + "_title").text(searchKey);
+                } else {
+                    for (var i = 0; i < ideaIds.length; i++) {
+                        $('#' + ideaIds[i]).attr('src', data.hits[i].webformatURL);
+                        $('#' + ideaIds[i] + "_title").text(searchKey);
+                    }
+                }
+                $grid.masonry('layout');
+            },
+            dataType: 'json',
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    }
+
+    $('#searchForm').on('submit', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        var searchText = $('#search__input').val();
+
+        replaceImages(searchText);
+    });
 
 });
