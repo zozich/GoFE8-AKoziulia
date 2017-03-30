b25ceab8930c649cad6844c8abf64c62bf7efc4f
Andrew Koziulia <akoziulia@cogniance.com>
2017-03-30 19:38:32 +0300

    JS final exam. Starting JS part

--- src/js/final/src/js/script.js
+++ src/js/final/src/js/script.js
@@ -1,10 +1,21 @@
 $(function () {
 
-    var $grid = $('.ideas__grid').imagesLoaded( function() {
-        $grid.masonry({
-            itemSelector: '.ideas__item'
-            // options...
-        });
+    var $grid = $('.ideas__grid').masonry({
+        itemSelector: '.ideas__item'
     });
+
+    $.ajax({
+        url: "https://pixabay.com/api/?key=4967317-cf3933732a04c09ee425d3446&q=yellow+flowers&image_type=photo",
+        success: function (data) {
+            console.log(data);
+        },
+        dataType: 'json',
+        error: function (error) {
+            console.log(error);
+        },
+        beforeSend: setHeaders
+    });
+
+    $grid.masonry('layout');
 
 });
